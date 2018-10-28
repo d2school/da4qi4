c3ca1d7612505d31d9ad179c2fad35b9f2e58611
zhuangyan <zhuangyan-stone@hotmail.com>
2018-10-28 19:50:36 +0800

    “captcha”

--- main.cpp
+++ main.cpp
@@ -3,16 +3,39 @@
 #include "server.hpp"
 #include "application.hpp"
 
+#include "utilities/captcha_utilities.hpp"
+
 int main()
 {
+    std::srand(std::time(nullptr));
     using namespace da4qi4;
-    
+
+    for (int i = 0; i < 100; ++i)
+    {
+        std::string captcha_text = Utilities::get_random_words_for_captcha(4, true);
+        std::cout << captcha_text << std::endl;
+        std::string fn;
+
+        for (auto c : captcha_text)
+        {
+            if (Utilities::is_symbol_captcha_char(c))
+            {
+                c = '_';
+            }
+
+            fn.push_back(c);
+        }
+
+        fn = "./tmp/" + std::to_string(i) + "-" + fn + ".jpg";
+        Utilities::make_captcha_image(captcha_text, fn);
+    }
+
+    return -1;
     boost::asio::io_context ioc;
-    
+
     try
     {
         auto svc = Server::Supply(ioc, 4099);
-        
         Application app1("d2school", "/");
         app1.AddHandler(_GET_, ""_router_starts, [](Context ctx)
         {
@@ -20,14 +43,12 @@ int main()
             ctx->Res().SetBody("<!DOCTYPE html><html lang=\"zh-cn\"><body><h1>D2SCHOOL</h1></body></html>");
             ctx->Bye();
         });
-        
         app1.AddHandler(_GET_, "test/"_router_starts, [](Context ctx)
         {
             ctx->Res().AddHeader("Context-Type", "text/html; charset=utf-8");
             ctx->Res().SetBody("<!DOCTYPE html><html lang=\"zh-cn\"><body><h2>D2SCHOOL/TEST</h2></body></html>");
             ctx->Bye();
         });
-        
         Application app2("d2school-admin", "/admin/");
         app2.AddHandler(_GET_, ""_router_starts, [](Context ctx)
         {
@@ -35,24 +56,20 @@ int main()
             ctx->Res().SetBody("<!DOCTYPE html><html lang=\"zh-cn\"><body><h1>D2SCHOOL-ADMIN</h1></body></html>");
             ctx->Bye();
         });
-        
         svc->AddApp(app1);
         svc->AddApp(app2);
-        
         svc->AddHandler(_GET_, "/admin/test/"_router_starts, [](Context ctx)
         {
             ctx->Res().AddHeader("Context-Type", "text/html; charset=utf-8");
             ctx->Res().SetBody("<!DOCTYPE html><html lang=\"zh-cn\"><body><h1>D2SCHOOL-ADMIN/TEST</h1></body></html>");
             ctx->Bye();
         });
-        
         svc->AddHandler(_POST_, "/"_router_starts, [](Context ctx)
         {
             ctx->Res().AddHeader("Context-Type", "text/html; charset=utf-8");
             ctx->Res().SetBody("<!DOCTYPE html><html lang=\"zh-cn\"><body><h1>D2SCHOOL-POST</h1></body></html>");
             ctx->Bye();
         });
-        
         svc->Start();
         ioc.run();
     }
@@ -60,6 +77,6 @@ int main()
     {
         std::cerr << e.what() << std::endl;
     }
-    
+
     return 0;
 }
--- utilities/captcha_utilities.hpp
+++ utilities/captcha_utilities.hpp
@@ -3,15 +3,22 @@
 
 #include <string>
 
-#include "captcha/captcha.hpp"
+namespace da4qi4
+{
+namespace Utilities
+{
 
-namespace da4qi4 
-{
-namespace Utilities 
-{
-    ;//std::string m   
-    
-} //namespace Utilities    
+extern size_t const max_captcha_size;
+extern bool is_symbol_captcha_char(char c);
+extern std::string get_random_words_for_captcha(size_t len, bool allow_symbol = false);
+
+extern void make_captcha_image(std::string const& captcha_text
+                               , std::string const& filename, bool add_border = false);
+extern void make_captcha_image(char const* captcha_text
+                               , size_t len
+                               , char const* filename, bool add_border = false);
+
+} //namespace Utilities
 } //namespace da4qi4
 
 #endif // CAPTCHA_UTILITIES_HPP
