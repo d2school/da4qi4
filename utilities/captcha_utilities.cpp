82f397eb800a3ba6112f298d7c1b2ca520146092
zhuangyan <zhuangyan-stone@hotmail.com>
2018-10-28 14:16:22 +0800

    use cimg, remove capture

---
+++ utilities/captcha_utilities.cpp
@@ -0,0 +1,11 @@
+#include "utilities/captcha_utilities.hpp"
+
+namespace da4qi4
+{
+namespace Utilities
+{
+
+
+} //namespace Utilities
+} //namespace da4qi4
+
---
+++ utilities/captcha_utilities.hpp
@@ -0,0 +1,17 @@
+#ifndef CAPTCHA_UTILITIES_HPP
+#define CAPTCHA_UTILITIES_HPP
+
+#include <string>
+
+#include "captcha/captcha.hpp"
+
+namespace da4qi4 
+{
+namespace Utilities 
+{
+    ;//std::string m   
+    
+} //namespace Utilities    
+} //namespace da4qi4
+
+#endif // CAPTCHA_UTILITIES_HPP
