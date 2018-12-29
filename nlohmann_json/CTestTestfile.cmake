d374eb34e60ec1f8b0593c0cfcd88cfc19c1304c
zhuangyan-stone <zhuangyan-stone@hotmail.com>
2018-12-29 11:29:24 +0800

    json update

--- nlohmann_json/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zhuangyan/projects/CPP/da4qi4/daqi/nlohmann_json
-# Build directory: /home/zhuangyan/projects/CPP/da4qi4/daqi/nlohmann_json
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
