25d4e93321f6fa389cb346fe1ad551b191594147
d2school <zhuangyan-stone@hotmail.com>
2020-01-28 20:54:15 +0800

    上传文件是否存储为临时文件判断条件合并为一行

--- src/application.cpp
+++ src/application.cpp
@@ -206,37 +206,11 @@ void ApplicationMgr::CheckTemplatesUpdate()
 bool UploadFileSaveOptions::IsNeedSave(std::string const& extension
                                        , size_t filesize_kb) const
 {
-    if (strategy == always_save)
-    {
-        return true;
-    }
-
-    if (strategy == alway_no_save)
-    {
-        return false;
-    }
-
-    if (strategy == size_greater_than)
-    {
-        return filesize_kb > size_base_kb;
-    }
-
-    if (strategy == size_lesser_than)
-    {
-        return filesize_kb < size_base_kb;
-    }
-
-    if (strategy == extension_is)
-    {
-        return extensions.find(extension) != extensions.cend();
-    }
-
-    if (strategy == extension_is_not)
-    {
-        return extensions.find(extension) == extensions.cend();
-    }
-
-    return false;
+    return (strategy == always_save)
+            || ((strategy == size_greater_than) && (filesize_kb > size_base_kb))
+            || ((strategy == size_lesser_than) && (filesize_kb < size_base_kb))
+            || ((strategy == extension_is) && (extensions.find(extension) != extensions.cend()))
+            || ((strategy == extension_is_not) && (extensions.find(extension) == extensions.cend()));
 }
 
 
